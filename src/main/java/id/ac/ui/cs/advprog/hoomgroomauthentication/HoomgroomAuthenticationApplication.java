fc8329b20fb167814ef345023908f18cb946deaf
shirinzarqaa <shirinzarqaa354@gmail.com>
2024-04-07 20:07:15 +0700

    initialized Spring

---
+++ src/main/java/id/ac/ui/cs/advprog/hoomgroomauthentication/HoomgroomAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.hoomgroomauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoomgroomAuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HoomgroomAuthenticationApplication.class, args);
+    }
+
+}
