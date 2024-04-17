924d027235c59bedc0166622178969a34e3c1304
Lidwina Eurora <lidwinaeurora@Lidwinas-MacBook-Air.local>
2024-04-17 20:11:03 +0700

    Add authentication request, response, and security handling classes

---
+++ src/main/java/id/ac/ui/cs/advprog/hoomgroomauthentication/request/SignupRequest.java
@@ -0,0 +1,65 @@
+package id.ac.ui.cs.advprog.hoomgroomauthentication.request;
+
+public class SignupRequest {
+
+    private String fullname;
+
+    private String dob;
+
+    private String sex;
+
+    private String username;
+
+    private String email;
+
+    private String password;
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
