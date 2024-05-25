39092cd08b87dfce144dd42a3d8e8ad209f49b4f
VirgilliaYeala <virgillia.yeala@ui.ac.id>
2024-05-25 09:42:24 +0700

    [REFACTOR] deleted any duplication

--- src/main/java/id/ac/ui/cs/advprog/hoomgroomauthentication/models/User.java
+++ src/main/java/id/ac/ui/cs/advprog/hoomgroomauthentication/models/User.java
@@ -1,8 +1,12 @@
 package id.ac.ui.cs.advprog.hoomgroomauthentication.models;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 @Table(name = "app_user")
 public class User {
 
@@ -24,62 +28,6 @@ public class User {
         this.sex = sex;
         this.username = username;
         this.email = email;
-        this.password = password;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFullname() {
-        return fullname;
-    }
-
-    public void setFullname(String fullname) {
-        this.fullname = fullname;
-    }
-
-    public String getDob() {
-        return dob;
-    }
-
-    public void setDob(String dob) {
-        this.dob = dob;
-    }
-
-    public String getSex() {
-        return sex;
-    }
-
-    public void setSex(String sex) {
-        this.sex = sex;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
         this.password = password;
     }
 
--- src/main/java/id/ac/ui/cs/advprog/hoomgroomauthentication/request/SignupRequest.java
+++ src/main/java/id/ac/ui/cs/advprog/hoomgroomauthentication/request/SignupRequest.java
@@ -1,5 +1,10 @@
 package id.ac.ui.cs.advprog.hoomgroomauthentication.request;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class SignupRequest {
 
     private String fullname;
@@ -13,53 +18,4 @@ public class SignupRequest {
     private String email;
 
     private String password;
-
-    public String getFullname() {
-        return fullname;
-    }
-
-    public void setFullname(String fullname) {
-        this.fullname = fullname;
-    }
-
-    public String getDob() {
-        return dob;
-    }
-
-    public void setDob(String dob) {
-        this.dob = dob;
-    }
-
-    public String getSex() {
-        return sex;
-    }
-
-    public void setSex(String sex) {
-        this.sex = sex;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
 }
