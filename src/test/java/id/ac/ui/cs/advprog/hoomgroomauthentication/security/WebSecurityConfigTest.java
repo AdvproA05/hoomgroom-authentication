de98e9b0b0dd129ebed68908dec202a5e00c9bd5
Lidwina Eurora <lidwinaeurora@Lidwinas-MacBook-Air.local>
2024-05-26 16:57:19 +0700

    [REFACTOR] Add more tests for code coverage

--- src/test/java/id/ac/ui/cs/advprog/hoomgroomauthentication/security/WebSecurityConfigTest.java
+++ src/test/java/id/ac/ui/cs/advprog/hoomgroomauthentication/security/WebSecurityConfigTest.java
@@ -8,20 +8,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.AuthenticationEntryPoint;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.test.util.ReflectionTestUtils;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import static org.mockito.Mockito.*;
-import static org.springframework.security.config.Customizer.withDefaults;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @ExtendWith(MockitoExtension.class)
 class WebSecurityConfigTest {
@@ -38,16 +29,31 @@ class WebSecurityConfigTest {
     @InjectMocks
     private WebSecurityConfig webSecurityConfig;
 
+    private final WebSecurityConfig webSecurityConfigs = new WebSecurityConfig();
+
     @Test
     void passwordEncoderBeanCreated() {
         PasswordEncoder passwordEncoder = webSecurityConfig.passwordEncoder();
-        assert passwordEncoder != null;
+        assertNotNull(passwordEncoder);
     }
 
     @Test
     void authenticationJwtTokenFilterBeanCreated() {
         AuthTokenFilter authTokenFilter = webSecurityConfig.authenticationJwtTokenFilter();
-        assert authTokenFilter != null;
+        assertNotNull(authTokenFilter);
+    }
+
+    @Test
+    void authenticationManagerBeanCreated() throws Exception {
+        AuthenticationConfiguration authenticationConfiguration = mock(AuthenticationConfiguration.class);
+
+        AuthenticationManager authenticationManager = mock(AuthenticationManager.class);
+
+        when(authenticationConfiguration.getAuthenticationManager()).thenReturn(authenticationManager);
+
+        AuthenticationManager actualAuthenticationManager = webSecurityConfigs.authenticationManager(authenticationConfiguration);
+
+        assertNotNull(actualAuthenticationManager);
     }
 
     @Test
